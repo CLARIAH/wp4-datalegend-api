b6287eb31d299ed44dbb585b4fcfb67fac4fc386
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-04 20:40:17 +0100

    SocketIO stuff

--- src/app/config_template.py
+++ src/app/config_template.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
-## Copy this file to 'config.py' and make necessary changes for your local setup.
+# Copy this file to 'config.py' and make necessary changes for your local setup.
 import os
 
-base_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"<RELATIVE PATH TO DATA FOLDER>"))
+base_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "<RELATIVE PATH TO DATA FOLDER>"))
 
 # SPARQL Endpoint Configuration
 ENDPOINT_URL = '<URL OF SPARQL ENDPOINT>'
@@ -14,7 +13,17 @@ CRUD_URL = '<CRUD URL>'
 from requests.auth import HTTPDigestAuth
 CRUD_AUTH = HTTPDigestAuth('<USER>', '<PASS>')
 
+# Is the API running behind a proxy?
+BEHIND_PROXY = False
+
+# Should the API log at DEBUG level?
+DEBUG = True
+
 # Stardog specific stuff
 # (these are the default settings for HTTP Basic authenticating)
 REASONING_TYPE = 'NONE'
 AUTH = ('admin','admin')
+
+# Respond to GitHub webhooks
+FOLLOW_GITHUB = True
+FOLLOW_BRANCH = 'master'
