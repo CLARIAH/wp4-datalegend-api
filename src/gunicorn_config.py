34df43ae57ec2e1f01e013a7b42579663bf92996
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-05 09:56:40 +0100

    Fixed gunicorn (apparently flask-socketio does not work very well with multiple workers in unicorn)

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -1,9 +1,10 @@
 workers = 1
-worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
+worker_class = 'gevent'
 bind = '0.0.0.0:5000'
 pidfile = '/tmp/gunicorn-csdh-api.pid'
 debug = True
-loglevel = 'debug'
+reload = True
+loglevel = 'info'
 errorlog = '/tmp/gunicorn_csdh_api_error.log'
 accesslog = '/tmp/gunicorn_csdh_api_access.log'
 daemon = False
