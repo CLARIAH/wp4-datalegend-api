38cbd3868a489a573cdb6600e5adf4dd1b55ee76
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-05 08:47:31 +0100

    New requirements, added gunicorn config

---
+++ src/gunicorn_config.py
@@ -0,0 +1,9 @@
+workers = 1
+worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
+bind = '0.0.0.0:5000'
+pidfile = '/tmp/gunicorn-csdh-api.pid'
+debug = True
+loglevel = 'debug'
+errorlog = '/tmp/gunicorn_csdh_api_error.log'
+accesslog = '/tmp/gunicorn_csdh_api_access.log'
+daemon = False
